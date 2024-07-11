4c2c70a36ae78dca36de322b32758b7f27e106bc
PabloAnjos <pablomatheussilvaanjoss@gmail.com>
2024-07-11 15:07:11 -0300

    lampada 1.0

---
+++ index.js
@@ -0,0 +1,9 @@
+function lampadaAcessa() {
+  let image = document.getElementById('image').src = 'https://raw.githubusercontent.com/gabrieldarezzo/helpjs-ravi/master/images/lampada-on.jpg';
+}
+
+function lampadaApagada() {
+  let image2 = document.getElementById('image').src = 'https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada.jpg?raw=true';
+}
+
+
